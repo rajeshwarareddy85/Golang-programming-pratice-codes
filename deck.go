913c7ebbfba1b694744690396344c87368c6c8ac
rajeshwara reddy <rajeshwarareddy85@gmail.com>
2026-08-12 10:31:44 +0000

    messag

--- deck.go
+++ deck.go
@@ -4,8 +4,25 @@ import "fmt"
 
 type deck []string
 
+func newdeck() deck {
+    cards:=deck{}
+	cardsuits:=[]string{"spades","clubs","diamonds","hearts"}
+	cardsnum:=[]string{"ace","2","3","4","5","6"}
+
+	for _,sym:=range cardsuits{
+		for _,num:=range cardsnum{
+			cards=append(cards,sym+" of "+num)
+		}
+	}
+	return cards
+}
+
 func (d deck) print(){
 	for i,card:=range d{
 		fmt.Println(i,card)
 	}
+}
+
+func deal(d deck,handsize int)(deck, deck){
+	return d[:handsize],d[handsize:]
 }
--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
-
+import "fmt"
 func main(){
-	cards:=deck{"spades","clubs","diamonds","hearts"}
-	cardsnum:=deck{"ace","2","3","4","5","6"}
-
-	for _,sym:=range cards{
-		for _,num:=range cardsnum{
-			cards=append(cards,sym+" of "+num)
-		}
-	}
-	cards.print()
+	cards:= newdeck()
+	hand,remainingdeck:=deal(cards,5)
+	hand.print()
+	fmt.Println("remaining cards contain:")
+	remainingdeck.print()
 	
 }
