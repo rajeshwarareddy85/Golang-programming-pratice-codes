0e29b862a46ee962142183935a91a69d6f836c6e
rajeshwara reddy <rajeshwarareddy85@gmail.com>
2026-08-13 11:07:19 +0000

    message

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
-import "fmt"
+
+
+
 func main(){
 	cards:= newdeck()
-	hand,remainingdeck:=deal(cards,5)
-	hand.print()
-	fmt.Println("remaining cards contain:")
-	remainingdeck.print()
-	
+	// hand,remainingdeck:=deal(cards,5)
+	// hand.print()
+	// fmt.Println("remaining cards contain:")
+	// remainingdeck.print()
+    cards.saveToFile("my_cards")
+
 }
